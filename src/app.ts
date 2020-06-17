737a91eec961fe0d026b26631a486bff5df4a803
Akash Perera | PickMe <akash@pickme.lk>
2020-06-17 17:01:36 +0530

    New: .env example added.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import Logger from './domain/logger';
 import {port} from './config/config';
 import routes from './http/routes';
 
-routes.listen(port, () => {
-    Logger.info(`server running on port : ${port}`);
-}).on('error', (e) => Logger.error(e));
+if (port !== undefined) {
+    routes.listen(port, () => {
+        Logger.info(`server running on port : ${port}`);
+    }).on('error', (e) => Logger.error(e));
+} else {
+    Logger.error(".env file missing")
+    process.exit()
+}
+
